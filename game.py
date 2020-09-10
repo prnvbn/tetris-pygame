1c49662ee9b2391bebccf93855c56b98f044d15a
pb719 <pb719@ic.ac.uk>
2020-09-11 05:06:47 +0530

    Add create_grid()

--- game.py
+++ game.py
@@ -1,5 +1,27 @@
 import defs as ds
 
-def create_grid(locked_positions={}):
-    # initialising a 20 x 10 grid
-    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
+def create_grid(locked_positions={}, hieght=20, width=20):
+    """Creates a Grid!
+
+    Parameters
+    ----------
+
+    locked_positions: dict{(i, j) : (r, g, b)}
+        dictionary of position, colour pair as key value pairs
+    hieght: int
+        grid hieght, default is 20    
+    width: int
+        grid width, default is 10    
+    """
+    # initialising a black 20 x 10 grid
+    # 20 rows and 10 colums
+    grid = [[(0, 0, 0) for _ in range(hieght)] for _ in range(width)]
+
+    # Colouring the locked positions
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if (col, row) in locked_positions:    
+                colour = locked_positions[(col, row)]
+                grid[row][j] = colour
+    return grid
+
